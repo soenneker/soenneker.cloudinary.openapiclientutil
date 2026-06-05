05fbf4cb5dbd4815a811d01852f09d4ce55d0419
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:05 -0500

    xml commenting

--- src/Soenneker.Cloudinary.OpenApiClientUtil/Abstract/ICloudinaryOpenApiClientUtil.cs
+++ src/Soenneker.Cloudinary.OpenApiClientUtil/Abstract/ICloudinaryOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Cloudinary.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface ICloudinaryOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<CloudinaryOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
